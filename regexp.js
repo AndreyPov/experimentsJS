0dd384365d0694b15debb1121ede485eedd11207
AndreyPov <povoroznuka.m@gmail.com>
2017-10-17 01:44:38 +0300

    grouping, bounderies, conditionals, replace

--- regexp.js
+++ regexp.js
@@ -26,7 +26,7 @@ console.log("Check ^[1-9] for 0abs0: " + /[^1-9]/.test("0abs0") + "\n" + "Check 
 // May be repeated more than once
 console.log("Check for +. 'Can be repeated more than once'(123): " + /\d+/.test("123"));
 console.log("Check for +. 'Can be repeated more than once'(2bv): " + /\d+/.test("2bv"));
-console.log("Check for *. 'Can be repeated more than once including zero'(2bv): " + /\d*/.test("\nsdf "));
+console.log("Check for *. 'Can be repeated more than once including zero'(2bv): " + /\d*/.test("2bv"));
 
 //? mark experiments
 let neighbor = /neighbou?r/;
@@ -37,3 +37,31 @@ console.log("Neighbor: " + neighbor.test("neighbor"));
 let dateTime = /\d{1,2}-\d{1,2}-\d{4}/;
 console.log("Test for 30-8-1992: " + dateTime.test("30-8-1992"));
 console.log("Test for 22-1992: " + dateTime.test("22-1992"));
+
+//grouping subexpression&case insensivity
+let repeated = /woo+woo+(woof)+/i;
+console.log("Test for grouping(woo+woo+(woof)+): " + repeated.test("woooowooooooooooowoofwoofwoof"));
+
+//exec takes first found match
+let match=/\d+/.exec("one,two, 10213123 sfasdfasd 232");
+console.log("Found number: " + match[0]);
+console.log("Index of it: " + match.index);
+
+//Simply the same is for string method 'match'
+let str = "Dude man, that is amazing!".match(/ \w{2} /);
+console.log("Matching for 'Dude man, that is amazing!'" + str[0]);
+
+//puzzle
+var quotedText = /'[^']*'/;
+console.log(quotedText.exec("she said 'hello'")[0]);
+console.log(/bad(ly)?/.exec("badly")[0]);
+console.log(/(\d)+/.exec("123")[0]);
+
+//boundaries
+console.log("Bounderies '\b' is like '^\d': " + /\bcat\b/.test("con\ncat\nenate"));
+
+//conditions
+console.log('15 students'.match(/\d{1,4} (student|teacher|cow|pupil)s?/)[0]);
+
+//replace method + using global for repeats + insensitive
+console.log("Chunga-changa".replace(/(ch)/gi,"T"));
